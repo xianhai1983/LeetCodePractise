de335faf5bcc936ed224cc99132f7fdc45e47693
xianhai1983 <xianhai1983@gmail.com>
2020-05-28 22:43:03 +1200

    Create Solution.java

---
+++ 141/Solution.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next ==null) return false;
+        ListNode slow = head.next;
+        ListNode fast = head.next.next;
+        while(fast!=null) {
+            if (slow == fast) return true;
+            slow = slow.next;
+            fast = fast.next;
+            if (fast == null) return false;
+            fast = fast.next;
+        }
+        return false; 
+    }
+}
