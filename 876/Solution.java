bacc9bf8d92bee85b094d75c845e701f7ea33f62
xianhai1983 <xianhai1983@gmail.com>
2020-05-30 00:02:31 +1200

    Create Solution.java

---
+++ 876/Solution.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        if (head.next == null) return head;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!= null) {
+            fast = fast.next;
+            if (fast == null) break;
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+}
